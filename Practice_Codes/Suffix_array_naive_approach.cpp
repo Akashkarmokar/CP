f2f0a1239e8fee7c180755d2b289b2e735440659
Akashkarmokar <akashcsemu@gmail.com>
2020-06-30 22:52:45 +0600

    .

---
+++ Practice_Codes/Suffix_array_naive_approach.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<string,int> >v;
+int main()
+{
+    string s = "missisippi";
+    for(int i=0;i<s.size();i++)
+    {
+        v.push_back(make_pair(s.substr(i),i));
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i].second<<" "<<v[i].first<<endl;
+    }
+    return 0;
+}
+
+// Complexity : O(n^2log(n)) 
