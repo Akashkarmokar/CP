1f110bf3850d52ae2dc140bc2a2bad85ec13207e
Akashkarmokar <akashcsemu@gmail.com>
2020-10-02 16:16:22 +0600

    init

---
+++ Codeforces/CFR_674/a.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nl '\n'
+#define ll long long 
+#define ull unsigned long long
+#define debug(x)       { cerr << #x << " = " << x << endl; } 
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+ 
+const int Max = (int)2e5+5;
+ 
+//functions
+ll lcm(ll a,ll b) {if(a==0 || b==0){return 0;}else{return (a*b)/__gcd(a,b);}}
+ 
+//direction arry
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+ 
+//____________________________________________________________________________________________________
+
+void solve()
+{
+    int n,x;
+    cin>>n>>x;
+    if(n<=2)
+    {
+        cout<<"1"<<endl;
+    }
+    else
+    {
+        cout<<((n-3)/x)+2<<endl;
+    }
+}
+int main()
+{
+    fastread;
+    int tc=1;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+    return 0;
+}
