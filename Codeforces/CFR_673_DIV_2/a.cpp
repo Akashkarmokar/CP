e0b3d1be88b1a700b85f1e813a9a146d60d99fd2
Akashkarmokar <akashcsemu@gmail.com>
2020-10-04 01:16:58 +0600

    init

---
+++ Codeforces/CFR_673_DIV_2/a.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nl '\n'
+#define ll long long 
+#define ull unsigned long long
+#define debug(x)       { cerr << #x << " = " << x << endl; } 
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+ 
+const int Max = (int)2e5+5;
+ 
+//functions
+ll lcm(ll a,ll b) {if(a==0 || b==0){return 0;}else{return (a*b)/__gcd(a,b);}}
+ 
+//direction arry
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+ 
+//____________________________________________________________________________________________________
+
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    sort(v.begin(),v.end());
+    int ans = 0;
+    for(int i=1;i<n;i++)
+    {
+        int tmp = (k-v[i])/v[0];
+        ans+=tmp;
+    }
+    cout<<ans<<endl;
+}
+int main()
+{
+    fastread;
+    int tc=1;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+    return 0;
+}
