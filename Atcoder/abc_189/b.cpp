fa50d52c77c8a06b3684131733c2bfaa13fc144d
slow__rider <akashcsemu@gmail.com>
2021-02-03 14:22:49 +0600

    init

---
+++ Atcoder/abc_189/b.cpp
@@ -0,0 +1,54 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nl '\n'
+#define ll long long 
+#define ull unsigned long long
+#define debug(x)       { cerr << #x << " = " << x << endl; } 
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+ 
+const int Max = (int)2e5+5;
+ 
+//functions
+ll lcm(ll a,ll b) {if(a==0 || b==0){return 0;}else{return (a*b)/__gcd(a,b);}}
+int bigmod(int a,int b,int m) {if(b==0) { return 1; } int x = bigmod(a,b/2,m); x = (x*x)%m; if(b%2==1) {x = (x*a)%m;} return x; }
+ 
+//direction arry
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+ 
+//____________________________________________________________________________________________________
+
+void solve()
+{
+    int n,x;
+    cin>>n>>x;  
+    vector<int>v(n+1),vv(n+1);
+    double ans = 0;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>v[i]>>vv[i];
+        ans +=v[i]*vv[i];
+        if(ans>x*100)
+        {
+            cout<<i<<nl;
+            return ;
+        }
+        
+    }
+    cout<<"-1"<<nl;
+
+}
+int main()
+{
+    fastread;
+    int tc=1;
+    // cin>>tc;
+    while(tc--){
+        solve();
+    }
+    return 0;
+}
---
+++ Atcoder/abc_189/c.cpp
@@ -0,0 +1,54 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nl '\n'
+#define ll long long 
+#define ull unsigned long long
+#define debug(x)       { cerr << #x << " = " << x << endl; } 
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+ 
+const int Max = (int)1e5+5;
+ 
+//functions
+ll lcm(ll a,ll b) {if(a==0 || b==0){return 0;}else{return (a*b)/__gcd(a,b);}}
+int bigmod(int a,int b,int m) {if(b==0) { return 1; } int x = bigmod(a,b/2,m); x = (x*x)%m; if(b%2==1) {x = (x*a)%m;} return x; }
+ 
+//direction arry
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+ 
+//____________________________________________________________________________________________________
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<ll>v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+
+    ll ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        ll min_i2j = 100005;
+        for(int j=i;j<n;j++)
+        {
+            min_i2j = min(min_i2j,v[j]);
+            ans = max(ans,min_i2j*(j-i+1));
+        }
+    }
+    cout<<ans<<nl;
+}
+int main()
+{
+    fastread;
+    int tc=1;
+    // cin>>tc;
+    while(tc--){
+        solve();
+    }
+    return 0;
+}
