c4a9720792a15abe087e62785e6a1b24fc06af5f
akashkarmokar <akashcsemu@gmail.com>
2021-12-18 22:29:47 +0600

    Seive code modified

--- Library/Math_&_Miscellaneous/seive function.cpp
+++ Library/Math_&_Miscellaneous/seive function.cpp
@@ -1,44 +1,67 @@
-#define Max 10000007
+/*
+    Author : UntilOver
+    Topic: 
+    Prerequisite:
+    Algorithm:
+    TimeComplexity:
+*/
 
+
+
+
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define ll long long 
+#define ull unsigned long long
+#define debug(x)       { cerr << #x << " = " << x << endl; } 
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+const int Max = (int)2e5+5;
+ 
+//functions
+ll lcm(ll a,ll b) {if(a==0 || b==0){return 0;}else{return (a*b)/__gcd(a,b);}}
+int bigmod(int a,int b,int m) {if(b==0) { return 1; } int x = bigmod(a,b/2,m); x = (x*x)%m; if(b%2==1) {x = (x*a)%m;} return x; }
+ 
+//direction arry
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+ 
+//____________________________________________________________________________________________________
 int prime[Max];
-
-
-void sieve()
-
-{
-    
+void sieve(int n){
+	n = min(n,Max);
 	prime[0] = 1 ;
-
-        prime[1] = 1 ;
-    
-	for(int i = 4 ;i<Max;i+=2)
-    
-	{
-        
+    prime[1] = 1 ;
+	for(int i = 4 ;i<n;i+=2){
 		prime[i] = 1 ;
-    
 	}
-    
-	int root = sqrt(Max);
-    
-	for(int i = 3 ;i<root;i+=2)
-    
+	for(int i = 3 ;i*i<n;i+=2)
 	{
-        
-		if(prime[i]==0)
-        
+		if(prime[i]==0)   
 		{
-            
-			for(int j = i*i ;j<Max;j+=i)
-            
-			{
-                
+			for(int j = i*i ;j<Max;j+=i){
 				prime[j] =  1 ;
-            
 			}
-        
 		}
-    
 	}
+}
+ 
+void solve(){
+	int n; cin>>n;
 
+ 	sieve(n);   
+}
+ 
+int main(){
+    fastread;
+    int tc=1;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+    return 0;
 }
